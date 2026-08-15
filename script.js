9950e75d309589f1b1455070f379fc7583da0684
kara <kyle13909013@gmail.com>
2026-08-16 02:10:07 +0330

    add jQuery

---
+++ script.js
@@ -0,0 +1,14 @@
+//Why coose us part
+
+$(".headexplain-text").hide();
+
+$(".headexplain-header").click(function() {
+    $(".headexplain-text").slideToggle();
+});
+
+// blog part
+$(".blog-posts").slice(2).hide();
+
+$(".allpost-btn").click(function() {
+    $(".blog-posts").slice(2).fadeIn();
+});
